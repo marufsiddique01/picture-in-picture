efd4f9620bb223e3578ee1709641d94ef5f1cd5d
marufsiddique01 <marufsiddique71@yahoo.com>
2021-07-03 01:06:56 +0600

    used screen capture api

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("yesssss");
+const videoElement = document.getElementById("video");
+const button= document.getElementById("button");
+
+//todo
+//async function to pass media
+//prompt to select media stream, pass to video element, then play
+
+
+//Screen Capture API
+
+async function selectMediaStream(){
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+        
+    } catch (error) {
+        // catch errors
+        console.log("Woops, error here: " , error)
+    }
+}
+
+
+button.addEventListener('click', async()=> {
+    //disable the button
+    button.disabled = true;
+    //start picture in picture
+    await videoElement.requestPictureInPicture();
+    //reset button
+    button.disabled = false;
+
+});
+
+
+//on load
+selectMediaStream()
